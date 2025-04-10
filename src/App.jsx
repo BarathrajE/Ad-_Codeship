3a6fd7d735f3434c6239fb1e5b9c00ddd0ed1807
Rahulsubramanian06 <92245492+Rahulsubramanian06@users.noreply.github.com>
2025-04-10 18:13:53 +0530

    swiper and testimonials added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-
 import './App.css';
 import "./styles.css";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Portfolio_and_below
+import {Portfolio_and_below} from "./components/portfolio_and_below.jsx";
 function App() {
- 
-
   return (
-    <>
-     
-    </>
+    <div id='home'>
+     {/* Portfolio_and_below */}
+     <Portfolio_and_below />
+    </div>
   )
 }
-
 export default App
---
+++ src/components/portfolio_card.jsx
@@ -0,0 +1,27 @@
+export function Portfolio_card({
+  image,
+  card_title,
+  card_subtitle_1,
+  card_subtitle_2,
+  card_subtitle_3,
+}) {
+  return (
+    <div className="test position-relative pt-5">
+      <img src={image} className="port_image position-relative z-0" alt="" />
+      <p className="card_title hover_effect  position-absolute font_size_24 font-weight-600 text-white">
+        {card_title}
+      </p>
+      <div className="card_subtitle hover_effect position-absolute z-3 gap-2 d-flex">
+        <button className="font_size_10 font-weight-400 bg-white border-0 px-4 py-1 rounded-pill">
+          {card_subtitle_1}
+        </button>
+        <button className="font_size_10 font-weight-400 bg-white border-0 px-4 py-1 rounded-pill">
+          {card_subtitle_2}
+        </button>
+        <button className="font_size_10 font-weight-400 bg-white border-0 px-4 py-1 rounded-pill">
+          {card_subtitle_3}
+        </button>
+      </div>
+    </div>
+  );
+}
